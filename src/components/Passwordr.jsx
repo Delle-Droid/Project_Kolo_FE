9e4e03e8ca99d000b3aa737064dffe2bc6a2d0b2
Fadeyi Oladele <oladelef7@gmail.com>
2020-05-04 00:11:58 +0100

    First Commit

---
+++ src/components/Passwordr.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PasswordR() {
+    return <div>
+
+    </div>
+}
+
+export default PasswordR;
---
+++ src/components/Savings.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Savings() {
+    return <div>
+
+    </div>
+}
+
+export default Savings;
